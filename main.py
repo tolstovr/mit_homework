22429f4f0d9ba9f6ffa3526c7c6307b559df4aaa
Robert Tolstov <97231830+tolstovr@users.noreply.github.com>
2024-04-04 22:25:29 +0400

    Fibonacci (starts from 1)
    This program has evolved to a useful application that helps you find a fibonacci numbers!

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import __hello__
+# import __hello__
+
+def fib(n):
+    if n <= 0:
+        return "Неверный ввод"
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+    
+print(f"Your fibonacci number is {fib(int(input("N: ")))}")
