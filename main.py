10a711dd5b08a01adc5938aff6b5f30624404d2b
Robert Tolstov <97231830+tolstovr@users.noreply.github.com>
2024-04-04 22:33:17 +0400

    Now it goes from 0
    We need to now what if we try fib(0)!!!

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 def fib(n):
     if n <= 0:
         return "Неверный ввод"
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+    if n <= 2:
+        return n - 1
+    return fib(n-1) + fib(n-2)
     
 print(f"Your fibonacci number is {fib(int(input("N: ")))}")
